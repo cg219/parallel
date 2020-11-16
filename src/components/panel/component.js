95f11eb585f22fa96a6bc5bf3e3d44bae4045b32
Clemente <dev@imkreative.com>
2020-11-15 23:37:52 -0500

    Update to React Hooks (Im starting to really dislike Javascript classes)

--- src/components/panel/component.js
+++ src/components/panel/component.js
@@ -1,45 +1,32 @@
-import React, { Fragment, Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./styles";
 
-export class Panel extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            title: this.props.title,
-            body: this.props.body,
-            color: ''
-        }
+function Panel (props) {
+    var [title, setTitle] = useState(props.title);
+    var [body, setBody] = useState(props.body);
+    var [color, setColor] = useState(props.color);
+    var colorMap = {
+        orange: styles.Orange,
+        purple: styles.Purple,
+        pink: styles.Pink,
     }
+    var classes = [styles.Panel, colorMap[color]].join(' ');
 
-    componentDidMount() {
-    }
+    useEffect(updateProps, [props]);
 
-    componentDidUpdate() {
-        if (this.props.title !== this.state.title) {
-            this.setState({
-                title: this.props.title,
-                body: this.props.body,
-                color: this.props.color
-            })
-        }
-    }
+    return (
+        <section className={classes}>
+            <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{__html: body}}></div>
+        </section>
+    )
 
-    render() {
-        var colorMap = {
-            orange: styles.Orange,
-            purple: styles.Purple,
-            pink: styles.Pink,
-        }
-
-        let classes = [styles.Panel, colorMap[this.state.color]].join(' ');
-
-        return (
-            <section className={classes}>
-                <h1>{this.state.title}</h1>
-                <div dangerouslySetInnerHTML={{__html: this.state.body}}></div>
-            </section>
-        )
+    function updateProps() {
+        if (props.title != title) setTitle(props.title);
+        if (props.body != body) setBody(props.body);
+        if (props.color != color) setColor(props.color);
     }
 }
+
+export { Panel }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Panel } from './components/panel/component';
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: {
-                'home': { title: '', body: ' '},
-                'what-we-do': { title: '', body: ' '},
-                'who-we-are': { title: '', body: ' '},
-                'opportunities': { title: '', body: ' '}
-            }
-        }
-    }
-
-    componentDidMount() {
-        fetch(`https://us-central1-parallel-site.cloudfunctions.net/cms-get?type=pages`)
-            .then(response => response.json())
-            .then(response => {
-                this.setState({ data: response.data });
-            })
-            .catch(error => console.error(error))
-    }
-
-    render() {
-        return (
-            <main>
-                <Panel title={this.state.data.home.title} body={this.state.data.home.html} color='purple' />
-                <Panel title={this.state.data['what-we-do'].title} body={this.state.data['what-we-do'].html} color='orange' />
-                <Panel title={this.state.data['who-we-are'].title} body={this.state.data['who-we-are'].html} color='pink' />
-                <Panel title={this.state.data.opportunities.title} body={this.state.data.opportunities.html} color='purple' />
-            </main>
-        )
-    }
-}
 
 ReactDOM.render(
     <BrowserRouter>
@@ -44,3 +9,35 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('parallel')
 );
+
+function App (props) {
+    var [home, setHome] = useState({ title: '', body: ' '});
+    var [what, setWhat] = useState({ title: '', body: ' '});
+    var [who, setWho] = useState({ title: '', body: ' '});
+    var [opp, setOpp] = useState({ title: '', body: ' '});
+
+    useEffect(fetchData, []);
+
+    function fetchData() {
+        fetch(`https://us-central1-parallel-site.cloudfunctions.net/cms-get?type=pages`)
+            .then(response => response.json())
+            .then(response => {
+                let data = response.data;
+
+                setHome({ title: data.home.title, body: data.home.html })
+                setWhat({ title: data['what-we-do'].title, body: data['what-we-do'].html })
+                setWho({ title: data['who-we-are'].title, body: data['who-we-are'].html })
+                setOpp({ title: data.opportunities.title, body: data.opportunities.html })
+            })
+            .catch(error => console.error(error))
+    }
+
+    return (
+        <main>
+            <Panel title={home.title} body={home.body} color='purple' />
+            <Panel title={what.title} body={what.body} color='orange' />
+            <Panel title={who.title} body={who.body} color='pink' />
+            <Panel title={opp.title} body={opp.body} color='purple' />
+        </main>
+    )
+}
